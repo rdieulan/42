98d52f6582203e9006b67a432de19a926673a5aa
Raphael DIEULANGARD <rdieulan@e2r3p12.42.fr>
2016-01-22 19:30:34 +0100

    gnl final 1.1

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,61 +6,76 @@
 /*   By: rdieulan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:29:18 by rdieulan          #+#    #+#             */
-/*   Updated: 2016/01/21 19:26:42 by rdieulan         ###   ########.fr       */
+/*   Updated: 2016/01/22 19:23:51 by rdieulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		get_next_line(int const fd, char **line)
+int		display(char **stk, char **line, int fd, int i)
 {
-	static char **stock;
-	char		*buf;
-	int			i;
-	int			ret;
+	*line = ft_strsub(stk[fd], 0, i);
+	stk[fd] = ft_strsub(stk[fd], i + 1, ft_strlen(stk[fd]) - i);
+	return (1);
+}
 
-	if (fd < 0 || !line)
-		return (-1);
-	buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	if (!buf)
-		return (-1);
-	if (!stock)
-	{
-		stock = (char **)ft_memalloc(sizeof(char *) * 100);
-		if (!stock)
-			return (-1);
-	}
-	if (!stock[fd])
-		stock[fd] = "";
+int		display_last(char **stk, char **line, int fd, int i)
+{
+	*line = ft_strsub(stk[fd], 0, i);
+	stk[fd][0] = 0;
+	ft_strdel(&stk[fd]);
+	return (1);
+}
+
+int		reading(char **stk, char *buf, char **line, int fd)
+{
+	int	i;
+	int	ret;
+
+	i = 0;
 	while ((ret = read(fd, buf, BUFF_SIZE)) > -1)
 	{
 		if (ret > 0)
 		{
-			stock[fd] = ft_strjoin(stock[fd], ft_strsub(buf, 0 ,BUFF_SIZE));
+			stk[fd] = ft_strjoin(stk[fd], ft_strsub(buf, 0, BUFF_SIZE));
 			buf = ft_memset(buf, '\0', BUFF_SIZE);
 		}
-		if (ret == 0 && stock[fd][0] == 0)
+		if (ret == 0 && stk[fd][0] == 0)
 			return (0);
-		i = 0;
-		while (stock[fd][i])
+		while (stk[fd][i])
 		{
-			if (stock[fd][i] == '\n' || stock[fd][i] == '\0')
-			{
-				*line = ft_strsub(stock[fd], 0, i);
-				stock[fd] = ft_strsub(stock[fd], i + 1, ft_strlen(stock[fd]) - i);
-				return (1);
-			}
+			if (stk[fd][i] == '\n' || stk[fd][i] == '\0')
+				return (display(stk, line, fd, i));
 			i++;
 		}
 		if (i > 0 && ret == 0)
-		{
-			*line = ft_strsub(stock[fd], 0, i);
-			stock[fd][0] = 0;
-			return (1);
-		}
+			return (display_last(stk, line, fd, i));
 	}
-	if (ret == -1)
+	return (2);
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	static char **stk;
+	char		*buf;
+	int			i;
+
+	if (fd < 0 || !line)
 		return (-1);
-	return (0);
+	buf = (char *)ft_memalloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (!buf)
+		return (-1);
+	if (!stk)
+	{
+		stk = (char **)ft_memalloc(sizeof(char *) * 100);
+		if (!stk)
+			return (-1);
+	}
+	if (!stk[fd])
+		stk[fd] = "";
+	if ((i = reading(stk, buf, line, fd)) != 2)
+		return (i);
+	ft_strdel(&buf);
+	return (-1);
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: rdieulan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:21:22 by rdieulan          #+#    #+#             */
-/*   Updated: 2016/01/21 18:07:03 by rdieulan         ###   ########.fr       */
+/*   Updated: 2016/01/22 19:26:49 by rdieulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 51
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include "libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
 int		get_next_line(int const fd, char **line);
 

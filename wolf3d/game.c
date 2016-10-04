f639ad3aa0660b89088e13c4a9d185d010c14c02
rdieulan <raphael.dieulangard@gmai.com>
2016-10-04 10:36:43 +0000

    wolf++

--- wolf3d/game.c
+++ wolf3d/game.c
@@ -12,6 +12,37 @@
 
 #include "wolf.h"
 
+void	ray_draw(t_env *env, double dist)
+{
+	int	i;
+	int	height;
+	
+	dist *= cos(env->tmp_angle - env->ray_angle);
+	height = 1200 / dist;
+	i = 400 - height;
+	if (i < 0)
+		i = 0;
+	color_set(env);
+	if (env->shadow == 1)
+		shadow_set(env, dist);
+	while (i < 400 + height && i < 800 && i >= 0 && env->col < WIN_W)
+		draw(env, env->col, i++);
+	env->red = 0;
+	env->blue = 0;
+	env->green = 0;
+	while (i < 400 + height + 4 && i < 800)
+		draw(env, env->col, i++);
+	env->red = 0;
+	env->blue = 50;
+	env->green = 0;
+	while (i < 799 && i > 0 && env->col < 1280)
+	{	
+		draw(env, env->col, i++);
+		if (i % 3 == 0)
+			env->blue++;
+	}
+}
+
 int		is_wall(t_env *env, double i)
 {
 	double	tmpx;
@@ -20,10 +51,17 @@ int		is_wall(t_env *env, double i)
 	int		y;
 
 	env->color = 1;
-	tmpx = env->posx + ()
+	tmpx = env->posx + (env->rx * i);
+	x = (int)tmpx;
+	tmpy = env->posy + (env->ry * i);
+	y = (int)tmpy;
+	color_wall(env, x, y, i);
+	if (env->map[y] && env->map[y][x] && env->map[y][x] == '1')
+		return(1);
+	return(0);
 }
 
-void	check_collision(t_env *env)
+void	check_env(t_env *env)
 {
 	double	i;
 	int		wall;
@@ -37,7 +75,14 @@ void	check_collision(t_env *env)
 			env->posx + (env->rx * i) >= 0 &&
 			env->posy + (env->ry * i) >= 0 && wall == 0)
 	{
-		if ();
+		if (is_wall(env, i) == 1)
+		{
+			i -= 0.1;
+			while (is_wall(env, i) != 1)
+				i += 0.001;
+			a = 1;
+			ray_draw(env, i);
+		}
 	}
 }
 
@@ -58,7 +103,7 @@ void	ray_set(t_env *env)
 		env->rx = rx;
 		env->ry = ry;
 		env->tmp_angle = ray_angle;
-		check_collision(env);
+		check_env(env);
 		ray_angle += M_PI / 6 / WIN_W;
 		env->col++;
 	}
@@ -66,5 +111,5 @@ void	ray_set(t_env *env)
 
 void	game(t_env *env)
 {
-	
+	ray_set(env);
 }
